63ba2e9c3b796c7e5b9b696e4addef013c80af79
Matteo Banerjee <matteobanerjee@gmail.com>
2022-12-06 08:33:53 -0600

    Day 6

---
+++ day6.ml
@@ -0,0 +1,20 @@
+open! Core
+
+let first_n_unique n lst =
+  if List.length lst < n then false
+  else List.take lst n |> Set.of_list (module Char) |> fun s -> Set.length s = n
+
+let solve n signal =
+  let rec loop inpt acc idx =
+    if first_n_unique n acc then idx
+    else
+      match inpt with
+      | [] -> failwith "Reached end of signal"
+      | x :: xs -> loop xs (x :: acc) (idx + 1)
+  in
+  loop signal [] 0
+
+let () =
+  let signal = In_channel.read_all "./input6.txt" |> String.to_list in
+  printf "\nSolution Part 1: %d\n" @@ solve 4 signal;
+  printf "Solution Part 2: %d\n" @@ solve 14 signal
