d62e61e916894c896ff8fcf9c8aeae1a42d08d15
Matteo Banerjee <matteobanerjee@gmail.com>
2022-12-08 10:18:31 -0600

    Day 8 part 2

--- day8.ml
+++ day8.ml
@@ -1,60 +1,77 @@
 open! Core
 
-let ak = Bigarray.Int
-let al = Bigarray.c_layout
+let dims a =
+  let dimx = Array.length a in
+  let dimy = Array.length a.(0) in
+  (dimx, dimy)
 
-let mk_array lines =
-  Bigarray.Array2.init ak al (List.length lines)
-    (String.length @@ List.nth_exn lines 0)
-  @@ fun i j -> Int.of_string @@ String.slice (List.nth_exn lines i) j (j + 1)
-
-let _print_2d_array ba =
-  for i = 0 to Bigarray.Array2.dim1 ba - 1 do
-    printf "\n";
-    for j = 0 to Bigarray.Array2.dim2 ba - 1 do
-      printf "%d" @@ Bigarray.Array2.get ba i j
+let iter_array a f =
+  let dimx, dimy = dims a in
+  for i = 0 to dimx - 1 do
+    for j = 0 to dimy - 1 do
+      f (i, j) a.(i).(j)
     done
   done
 
-(* let print_1d_array ba1 = *)
-(*   for i = 0 to Bigarray.Array1.dim ba1 - 1 do *)
-(*     printf "%d," @@ Bigarray.Array1.get ba1 i *)
-(*   done *)
+let mk_array lines =
+  let dimx = List.length lines in
+  let dimy = String.length @@ List.nth_exn lines 0 in
+  let a = Array.make_matrix ~dimx ~dimy 0 in
+  iter_array a (fun (i, j) _ ->
+      let v = Int.of_string @@ String.slice (List.nth_exn lines i) j (j + 1) in
+      a.(i).(j) <- v);
+  a
 
 type loop_direction = Right | Left | Up | Down
 
-let rec loop ba needle direction i j =
-  let (nexti, nextj) = match direction with
-  | Right -> (i, j - 1)
-  | Left -> (i, j + 1)
-  | Up -> (i - 1, j)
-  | Down -> (i + 1, j)
-  in
-  let v = try Bigarray.Array2.get ba nexti nextj with Invalid_argument _ -> -1 in
-  if v = -1 then true 
-  else if v >= needle then false
-  else loop ba needle direction nexti nextj
-
 let is_visible ba ci cj =
-  let max_i = Bigarray.Array2.dim1 ba - 1 in
-  let max_j = Bigarray.Array2.dim2 ba - 1 in
-  let needle = ba.{ci, cj} in
-  if ci = max_i || ci = 0 || cj = max_j || cj = 0 then
-    true
-  else
-    loop ba needle Right ci cj || loop ba needle Left ci cj || loop ba needle Up ci cj || loop ba needle Down ci cj
+  let rec loop needle direction i j =
+    let nexti, nextj =
+      match direction with
+      | Right -> (i, j - 1)
+      | Left -> (i, j + 1)
+      | Up -> (i - 1, j)
+      | Down -> (i + 1, j)
+    in
+    match Option.try_with (fun () -> ba.(nexti).(nextj)) with
+    | None -> true
+    | Some n -> if n >= needle then false else loop needle direction nexti nextj
+  in
+  let nd = ba.(ci).(cj) in
+  loop nd Right ci cj || loop nd Left ci cj || loop nd Up ci cj
+  || loop nd Down ci cj
 
-let count_visible ba =
+let solve_part_1 ba =
   let count = ref 0 in
-  for i = 0 to Bigarray.Array2.dim1 ba - 1 do
-    for j = 0 to Bigarray.Array2.dim2 ba - 1 do
-      if is_visible ba i j then count := !count + 1;
-    done
-  done;
+  iter_array ba (fun (i, j) _ -> if is_visible ba i j then count := !count + 1);
   !count
+
+let score ba ci cj =
+  let rec loop needle direction i j =
+    let nexti, nextj =
+      match direction with
+      | Right -> (i, j - 1)
+      | Left -> (i, j + 1)
+      | Up -> (i - 1, j)
+      | Down -> (i + 1, j)
+    in
+    match Option.try_with (fun () -> ba.(nexti).(nextj)) with
+    | None -> 0
+    | Some n -> if n >= needle then 1 else 1 + loop needle direction nexti nextj
+  in
+  let nd = ba.(ci).(cj) in
+  loop nd Right ci cj * loop nd Left ci cj * loop nd Up ci cj
+  * loop nd Down ci cj
+
+let solve_part_2 ba =
+  let max_score = ref 0 in
+  iter_array ba (fun (i, j) _ ->
+      let s = score ba i j in
+      if s > !max_score then max_score := s);
+  !max_score
 
 let () =
   let lines = In_channel.read_lines "./input8.txt" in
   let in_array = mk_array lines in
-  printf "\nSolution Part 1: %d\n" @@ count_visible in_array;
-  printf "Solution Part 2: %d\n" 0
+  printf "\nSolution Part 1: %d\n" @@ solve_part_1 in_array;
+  printf "Solution Part 2: %d\n" @@ solve_part_2 in_array
