52cbaad175ffeb33c7e0e27fc9360ba77d39b28d
Matteo Banerjee <matteobanerjee@gmail.com>
2022-12-10 07:51:11 -0700

    Day 9 part 2

--- day9.ml
+++ day9.ml
@@ -1,34 +1,32 @@
 open! Core
 
-type position = int * int 
-[@@deriving show, sexp ] 
+type position = int * int [@@deriving show, sexp]
+type position_list = position list [@@deriving show]
 
 let position_cmp (a1, b1) (a2, b2) =
   let a_cmp = Int.compare a1 a2 in
   if a_cmp = 0 then Int.compare b1 b2 else a_cmp
 
 module PositionSet = Set.Make (struct
-  type t = position
-  [@@deriving sexp]
+  type t = position [@@deriving sexp]
 
   let compare = position_cmp
 end)
 
 type state = {
-  head_position : position;
-  tail_position : position;
+  head : position;
+  tails : position list;
   visited_set : PositionSet.t;
 }
 
-let initial_state =
+let initial_state n_tails =
   {
-    head_position = (0, 0);
-    tail_position = (0, 0);
+    head = (0, 0);
+    tails = List.init n_tails ~f:(fun _ -> (0, 0));
     visited_set = PositionSet.of_list [ (0, 0) ];
   }
 
 type direction = Up | Down | Left | Right
-(* type inst = direction * int *)
 
 let parse_inst line =
   match String.split ~on:' ' line with
@@ -49,37 +47,45 @@ let move_tail (hx, hy) (tx, ty) =
   let ydiff = hy - ty in
   match (xdiff, ydiff) with
   | 2, 0 -> (tx + 1, ty)
-  | 2, 1 -> (tx + 1, hy)
-  | 2, -1 -> (tx + 1, hy)
+  | 2, 1 -> (tx + 1, ty + 1)
+  | 2, 2 -> (tx + 1, ty + 1)
+  | 2, -1 -> (tx + 1, ty - 1)
   | -2, 0 -> (tx - 1, ty)
-  | -2, 1 -> (tx - 1, hy)
-  | -2, -1 -> (tx - 1, hy)
+  | -2, 1 -> (tx - 1, ty + 1)
+  | -2, -1 -> (tx - 1, ty - 1)
+  | -2, -2 -> (tx - 1, ty - 1)
   | 0, 2 -> (tx, ty + 1)
-  | 1, 2 -> (hx, ty + 1)
-  | -1, 2 -> (hx, ty + 1)
+  | 1, 2 -> (tx + 1, ty + 1)
+  | -1, 2 -> (tx - 1, ty + 1)
+  | -2, 2 -> (tx - 1, ty + 1)
   | 0, -2 -> (tx, ty - 1)
-  | 1, -2 -> (hx, ty - 1)
-  | -1, -2 -> (hx, ty - 1)
+  | 1, -2 -> (tx + 1, ty - 1)
+  | 2, -2 -> (tx + 1, ty - 1)
+  | -1, -2 -> (tx - 1, ty - 1)
   | _ -> (tx, ty)
 
+let move_tails head tails =
+  List.folding_map ~init:head tails ~f:(fun prev curr ->
+      let next = move_tail prev curr in
+      (next, next))
+
 let move_1 state direction =
-  let next_head = move_head state.head_position direction in
-  let next_tail = move_tail next_head state.tail_position in
-  {
-    head_position = next_head;
-    tail_position = next_tail;
-    visited_set = PositionSet.add state.visited_set next_tail;
-  }
+  let head = move_head state.head direction in
+  let tails = move_tails head state.tails in
+  let last_tail = List.last_exn tails in
+  { head; tails; visited_set = PositionSet.add state.visited_set last_tail }
 
 let rec exec_inst state (direction, n) =
   if n = 0 then state else exec_inst (move_1 state direction) (direction, n - 1)
 
-let solve_part_1 instructions =
-  let final_state = List.fold ~init:initial_state ~f:exec_inst instructions in
+let solve instructions n_tails =
+  let final_state =
+    List.fold ~init:(initial_state n_tails) ~f:exec_inst instructions
+  in
   PositionSet.length final_state.visited_set
 
 let () =
   let lines = In_channel.read_lines "./input9.txt" in
   let instructions = List.map ~f:parse_inst lines in
-  printf "\nSolution Part 1: %d\n" (solve_part_1 instructions);
-  printf "Solution Part 2: %d\n" 0
+  printf "\nSolution Part 1: %d\n" (solve instructions 1);
+  printf "Solution Part 2: %d\n" (solve instructions 9)
